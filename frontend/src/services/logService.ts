b181c59839af155495bff2d8e1b8df447af83249
GalBenShushan <galbenshushan5@gmail.com>
2025-03-04 16:31:06 +0200

    fix | prettier

--- frontend/src/services/logService.ts
+++ frontend/src/services/logService.ts
@@ -1,20 +1,16 @@
 export const fetchLogs = async (alertId: string) => {
-    const response = await fetch(
-      'https://njwinkshi5hyldkqq4srpuoxrm0weqer.lambda-url.us-east-1.on.aws',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ alert_id: alertId }),
-      }
-    );
-  
-    if (!response.ok) {
-      throw new Error('Failed to fetch logs');
-    }
-  
-    const data = await response.json();
-    return data.cloudtrail_logs;
-  };
-  
+  const response = await fetch("/api/logs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ alert_id: alertId }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch logs");
+  }
+
+  const data = await response.json();
+  return data.cloudtrail_logs;
+};
