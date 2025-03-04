df0dff44284e4135822fa310aef4a585f22a8b99
GalBenShushan <galbenshushan5@gmail.com>
2025-03-04 16:30:45 +0200

    feat | init functional ui components

--- frontend/src/components/AlertDropdown.tsx
+++ frontend/src/components/AlertDropdown.tsx
@@ -1,10 +1,18 @@
+import { Autocomplete, TextField } from "@mui/material";
+import { useAlertLog } from "../hooks/useAlertLog";
+import { AlertOptions } from "../enums/alerts";
 
 const AlertDropdown = () => {
+  const { selectedAlertId, handleAlertChange } = useAlertLog();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Autocomplete
+      value={selectedAlertId}
+      onChange={handleAlertChange}
+      options={Object.values(AlertOptions)}
+      renderInput={(params) => <TextField {...params} label="Select Alert" />}
+      disableClearable
+    />
+  );
+};
 
-export default AlertDropdown
+export default AlertDropdown;
--- frontend/src/components/LogTable.tsx
+++ frontend/src/components/LogTable.tsx
@@ -1,5 +1,54 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { useAlertLog } from "../hooks/useAlertLog";
+
 const LogTable = () => {
-  return <div></div>;
+  const { logs } = useAlertLog();
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Timestamp</TableCell>
+            <TableCell>Event Name</TableCell>
+            <TableCell>User</TableCell>
+            <TableCell>Instance ID</TableCell>
+            <TableCell>Current State</TableCell>
+            <TableCell>Source IP</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {logs.map((log, index) => (
+            <TableRow key={index}>
+              <TableCell>{log.timestamp}</TableCell>
+              <TableCell>{log.event_name}</TableCell>
+              <TableCell>{log.user_identity.userName}</TableCell>
+              <TableCell>
+                {log.response_elements?.instancesSet?.items.map((item: any) => (
+                  <div key={item.instanceId}>{item.instanceId}</div>
+                ))}
+              </TableCell>
+              <TableCell>
+                {log.response_elements?.instancesSet?.items.map((item: any) => (
+                  <div key={`${item.currentState}_${item.instanceId}`}>
+                    {item.currentState || "N/A"}
+                  </div>
+                ))}
+              </TableCell>
+              <TableCell>{log.source_ip}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default LogTable;
