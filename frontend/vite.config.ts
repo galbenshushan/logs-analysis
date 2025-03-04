28f3f19c8155f48d15793ab05e64621db3004275
GalBenShushan <galbenshushan5@gmail.com>
2025-03-04 16:26:44 +0200

    feat | mutate vite config to use proxify lmbda service

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api/logs": {
+        target:
+          "https://njwinkshi5hyldkqq4srpuoxrm0weqer.lambda-url.us-east-1.on.aws",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
