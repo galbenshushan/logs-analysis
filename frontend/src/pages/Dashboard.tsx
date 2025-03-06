529f86bffcd80baa0c53c35d2a8e87d805ff9f26
GalBenShushan <galbenshushan5@gmail.com>
2025-03-06 17:30:49 +0200

    feat | styles and refactor

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,39 @@
-import AlertDropdown from '../components/AlertDropdown';
-import LogTable from '../components/LogTable';
+import AlertDropdown from "../components/AlertDropdown";
+import LogFilters from "../components/LogsFilters";
+import LogTable from "../components/LogTable";
+import styled from "styled-components";
+import { useAlertLog } from "../hooks/useAlertLog";
+
+const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const ControlsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  gap: 16px;
+`;
 
 const Dashboard = () => {
+  const { logs } = useAlertLog();
   return (
-    <div>
-      <h1>Alert Log Dashboard</h1>
-      <AlertDropdown />
-      <LogTable />
-    </div>
+    <DashboardContainer>
+      <Title>Alert Log Dashboard</Title>
+      <ControlsContainer>
+        <AlertDropdown />
+        <LogFilters />
+      </ControlsContainer>
+      {logs.length > 0 && <LogTable />}
+    </DashboardContainer>
   );
 };
 
